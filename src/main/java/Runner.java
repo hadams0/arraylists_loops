f62636638b995a476f590815cc4a39dbc2bcb6ca
hadams0 <adamshannah1@icloud.com>
2023-07-10 19:54:28 +0100

    Extension complete

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -48,31 +48,98 @@ public class Runner {
 
 //        9. Print out all the islands using a for loop
 
-        for(String scottishIsland : scottishIslands){
+        for (String scottishIsland : scottishIslands) {
             System.out.println(scottishIsland);
         }
 
         System.out.println(scottishIslands);
 
 
-        
-
 //        NUMBERS
         List<Integer> numbers = new ArrayList<>();
         Collections.addAll(numbers, 1, 1, 4, 2, 7, 1, 6, 15, 13, 99, 7);
 
-        System.out.println("numbers: " + numbers);
+        System.out.println("Numbers: " + numbers);
 
 //        1. Print out a list of the even integers
+
+        ArrayList<Integer> evenNumbers = new ArrayList<>();
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                evenNumbers.add(number);
+            }
+        }
+        System.out.println(evenNumbers);
+
 //        2. Print the difference between the largest and smallest value
+
+        int highestNumber = numbers.get(0);
+        int lowestNumber = numbers.get(0);
+
+        for (int number : numbers) {
+            if (number < lowestNumber) {
+                lowestNumber = number;
+            }
+
+            if (number > highestNumber) {
+                highestNumber = number;
+            }
+        }
+
+        System.out.println(highestNumber - lowestNumber);
+
 //        3. Print True if the list contains a 1 next to a 1 somewhere.
-//        4. Print the sum of the numbers,
+
+        boolean isPrevNumOne = false;
+
+        for (int number : numbers) {
+
+            if (isPrevNumOne && number == 1) {
+                System.out.println("True");
+            }
+
+            if (number == 1) {
+                isPrevNumOne = true;
+            } else {
+                isPrevNumOne = false;
+            }
+        }
+
+        // or this method is simpler maybe
+        int prevNum = -1;
+
+        for (int number : numbers) {
+            if (number == 1 && prevNum == 1) {
+                System.out.println("True");
+            }
+            prevNum = number;
+        }
+
+//        4. Print the sum of the numbers
+
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        System.out.println("Sum of numbers in list: " + sum);
+
 //        5. Print the sum of the numbers...
 //           ...except the number 13 is unlucky, so it does not count...
 //           ...and numbers that come immediately after a 13 also do not count.
 //
 //          So [2, 7, 13, 2] would have sum of 9.
 
+        int newSum = 0;
+        int previousNumber = -1;
+
+        for (int number : numbers){
+            if (number !=13 && previousNumber !=13) {
+                newSum += number;
+            }
+            previousNumber = number;
+        }
+
+        System.out.println("Sum = " + newSum);
     }
 
 }
